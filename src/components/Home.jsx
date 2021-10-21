a20c903cc11c91808eb293bc43b22bcd5e518e90
Katherine-valcarce <katherine.v@live.cl>
2021-10-21 18:30:55 -0300

    etiquetas vista 1 y 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
+            <img></img>
+            <h1>Guía de cumplimiento de Estándar SDDC</h1>
+            <img></img>
+            <Link to="/menu">
+            Ingresar </Link>
             
         </div>
     )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Menu = () => {
     return (
         <div>
-            
+            <img></img>
+            <button>Calculadora de Espacio Libre de Caída</button>
+            <button>Verificación de Arnés y Punto de Anclaje</button>
         </div>
     )
 }
