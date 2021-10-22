5512fa0d99487a07cec2f7af6d149dbfa44227dd
Hiliana Angulo <ahiliana@gmail.com>
2021-10-22 11:55:28 -0300

    Creacion de mas componentes y funcionalidad de estos

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
-const Checklist = () => {
+const Checklist = ({setCheckList}) => {
     return (
         <div>
-            
+            <h1>Estás en CheckList</h1>
+            <Link to='/results'>
+            <button
+            onClick={() => setCheckList(true) }
+            >
+                Enviar
+                </button>
+            </Link>
         </div>
     )
 }
