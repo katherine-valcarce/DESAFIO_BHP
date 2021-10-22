092440111bd80945c3b34e7b5b09d98041215bce
Katherine-valcarce <katherine.v@live.cl>
2021-10-22 12:34:38 -0300

    correccion flujo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const Menu = () => {
     return (
         <div>
             <img></img>
-            <button>Calculadora de Espacio Libre de Caída</button>
-            <button>Verificación de Arnés y Punto de Anclaje</button>
+            <Link to='/calculator'> <button>Calculadora de Espacio Libre de Caída</button> </Link>
+            <Link to='/checklist'><button>Verificación de Arnés y Punto de Anclaje</button> </Link>
         </div>
     )
 }
