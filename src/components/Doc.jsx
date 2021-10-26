fd4b453ef792f2953876365e808cf115061b8168
Katherine-valcarce <katherine.v@live.cl>
2021-10-26 15:46:33 -0300

    configuracion PDF

--- src/components/Doc.jsx
+++ src/components/Doc.jsx
@@ -1,63 +1,82 @@
-import React from 'react'
-import Pregunta1 from "../asset/checklist/Pregunta1.png"
-import Pregunta2 from "../asset/checklist/Pregunta2.png"
-import Pregunta3 from "../asset/checklist/Pregunta3.png"
-import Pregunta4 from "../asset/checklist/Pregunta4.png"
-import monitoescalada from "../asset/grafic.png"
+import React from "react";
+import Logo from "../asset/bhp-logo 2.png"
+import Question1 from "../asset/checklist/harnessDoc.png";
+import Question2 from "../asset/checklist/accesoriesDoc.png";
+import Question3 from "../asset/checklist/ropeDoc.png";
+import Question4 from "../asset/checklist/anclajeDoc.png";
+import Draw from "../asset/grafic.png";
 import { jsPDF } from "jspdf";
-import html2canvas from 'html2canvas'
-
+import html2canvas from "html2canvas";
+ 
 const Doc = (props) => {
-    const {user, position} = props;
-
-    const printDocument = () => {
-        const input = document.getElementById('divToPrint');
-        html2canvas(input)
-          .then((canvas) => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF();
-            pdf.addImage(imgData, 'JPEG', 0, 0);
-            pdf.save("download.pdf");
-          })
-        ;
-      }
-
-    return (
-      <div id='divToPrint'>          
-            <h1> Nombre de usuario : {user} </h1>
-            <h1> Nombre de cargo : {position} </h1>
-            <h1> Fecha y hora de emisión :  </h1>
-            <hr />
-            <h1> CHECKLIST ARNÉS DE SEGURIDAD Y PUNTOS DE ANCLAJE</h1>
-            <img src= {Pregunta1} alt= "pregunta1"></img>
-            <img src= {Pregunta2} alt= "pregunta2"></img>
-            <img src= {Pregunta3} alt= "pregunta3"></img>
-            <img src= {Pregunta4} alt= "pregunta4"></img>
+  const { user, position } = props;
+ 
+  const printDocument = () => {
+    const input = document.getElementById("divToPrint");
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 25, 10, 160, 280); //margen izq, margen top, width heigth
+      pdf.save("download.pdf");
+    });
+  };
+ 
+  return (
+    <>
+    <div id="divToPrint">
+      <div className="imgLeft">
+      <img className="logoDoc" src= {Logo} alt="logo" />
+      </div>
+      <h1 className="titleDoc">
+        Validación Guía de Cumplimiento de Estándar SDDC
+      </h1>
+      <h2 className="textDoc"> Nombre de usuario : {user} </h2>
+      <h2 className="textDoc"> Nombre de cargo : {position} </h2>
+      <h2 className="textDoc"> Fecha y hora de emisión : </h2>
+      <hr />
+      <h5 className="titleDoc2">
+        CHECKLIST ARNÉS DE SEGURIDAD Y PUNTOS DE ANCLAJE
+      </h5>
+      <div className="containerAnswer">
+        <img src={Question1} className="correctAnswer" alt="Question1"></img>
+        <img src={Question2} className="correctAnswer" alt="Question2"></img>
+        <img src={Question3} className="correctAnswer" alt="Question3"></img>
+        <img src={Question4} className="correctAnswer" alt="Question4"></img>
+      </div>
+ 
+      <pagebreak />
+      <h5 className="titleDoc2">CALCULO DE ESPACIO LIBRE DE CAÍDA</h5>
+      <div className="result">
         <div>
-            <h1>CALCULO DE ESPACIO LIBRE DE CAÍDA</h1>
-            <div>
-                <div>
-                    <h2>LE = </h2>
-                    <h2>EA = </h2>
-                    <h2>ET =</h2>
-                    <h2> MS =</h2>
-                </div>
-                <div>
-                    <h2>El resultado del ELC permite realizar el trabajo en altura, de acuerdo al estándar</h2>
-                </div>
-
-                <img src= {monitoescalada} alt= "monitoEscalada"></img>
-            </div>
-            <div>
-            <hr width= "300" size = "5"/>
-            <h1>Firma y Timbre </h1>
-            <h1> {user} </h1>
-            <h1> {position} </h1>
-            </div>
+          <h2 className="textDoc">LE=</h2>
+          <h2 className="textDoc">EA=</h2>
+          <h2 className="textDoc">ET=</h2>
+          <h2 className="textDoc">MS=</h2> <br />
+ 
+          <h2 className="textDoc">RESULTADO ELC=</h2>
         </div>
-          <button onClick={printDocument}>Descargar PDF</button>
-        </div>
-        
-    )
-}
-export default Doc
+        <div></div>
+        <img src={Draw} alt="Draw"></img>
+      </div>
+      <br />
+      <h5 className="titleDoc2">
+        El resultado del ELC permite realizar el trabajo en altura, de acuerdo
+        al estándar
+      </h5>
+      <div className="containerBtn">
+        <hr width="120" size="5" />
+        <h1 className="textDoc">Firma y Timbre </h1>
+        <h1> {user} </h1>
+        <h1> {position} </h1>
+      </div>
+    </div>
+    <div className="containerBtn">
+      <button className="btn btn-primary" onClick={printDocument}>
+          Descargar PDF
+        </button>
+      </div>
+</>
+      
+  );
+};
+export default Doc;
