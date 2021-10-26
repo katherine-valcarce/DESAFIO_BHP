73d7a66804a21996d4e9deccadba5870cd884d11
Katherine-valcarce <katherine.v@live.cl>
2021-10-26 14:23:02 -0300

    Se renderiza flujo de codigo

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,37 +1,10 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import ResultsELC from './ResultsELC'
 
 
 const Results = ({estrobo, height, space, informationELC, onChangeinformationELC}) => {
     return (
         <div>
-            {informationELC === 'Estarás a salvo' ?
-            <div>
-                <ResultsELC
-                   estrobo={estrobo}
-                   height={height}
-                   space={space}
-                   informationELC={informationELC}
-                   onChangeinformationELC={onChangeinformationELC}
-                 />
-               </div>
-             :
-             <div>
-                 <ResultsELC
-                 estrobo={estrobo}
-                 height={height}
-                 space={space}
-                 informationELC={informationELC}
-                 onChangeinformationELC={onChangeinformationELC}
-                />
-                 <Link to='/calculator'>
-                 <button>Realiza los calculos nuevamente</button>
-                 </Link>
-             </div>
-            
-        }
-        
+ 
         </div>
     )
 }
