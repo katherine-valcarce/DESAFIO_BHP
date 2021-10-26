3c23231145128d7ece66fe7806be2f27514bea4b
EstefanyVergara <vteffa@gmail.com>
2021-10-26 16:38:43 -0300

    Correct modal for all components

--- src/components/LifeRopeConditionWithAnswerYes.jsx
+++ src/components/LifeRopeConditionWithAnswerYes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import Remove from "../asset/remove.png";
 
 const LifeRopeConditionWithAnswerYes = ({condition,additionOfLifeRopeConditions, estadoDeCuerdaDeVida }) => {
     return (
@@ -31,44 +32,39 @@ const LifeRopeConditionWithAnswerYes = ({condition,additionOfLifeRopeConditions,
               >
                 {/* Obs. Con el atributo 'data-bs-backdrop="static"' obligamos a que sólo podamos cerrar el modal cliceando dentro de él */}
                 {/* Caja de diálogo */}
-                <div class="modal-dialog modal-dialog-centered">
-                  {" "}
-                  {/* modal-dialog-centered:centra verticalmente */}
-                  {/* Contenido de la caja */}
-                  <div class="modal-content">
-                    {/* Encabezado de la caja */}
-                    <div class="modal-header">
-                      <h5 class="modal-title">Título del modal</h5>
-                      <Link to='/menu'>
+                <div className="modal-dialog modal-dialog-centered">
+                {" "}
+                {/* modal-dialog-centered:centra verticalmente */}
+                {/* Contenido de la caja */}
+                <div className="modal-content">
+                  {/* Encabezado de la caja */}
+                  <div className="closedTag">
+                    <Link to="/menu">
                       <button
-                        class="btn-close"
+                        className="btn-close"
                         data-bs-dismiss="modal"
                         aria-label="cerrar"
-                        
                       ></button>
-                      </Link>
-                    </div>
-                    {/* Cuerpo de la caja */}
-                    <div class="modal-body">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Sed delectus esse fugiat quam culpa asperiores quo
-                        deleniti numquam quas laboriosam.
-                      </p>
-                    </div>
+                    </Link>
+                  </div>
+                  <div className="modal-header">
+                    <img className="remove" src={Remove} alt="remove" />
+                  </div>
+                  {/* Cuerpo de la caja */}
+                  <b className="modal-title text-center">¡ADVERTENCIA!</b>
+                  <div className="modal-body text-center">
+                    <b className="textModal">
+                      NO CUMPLE CON LOS ESTÁNDARES DE SEGURIDAD.
+                    </b>
+                    <br/>
+                    <p className="textModal">
+                      <u>Contacte con supervisor de faena </u> y revise
+                      nuevamente la seguridad de su equipamiento.
+                    </p>
+                  </div>
+                  <div className="modal-footer"></div>
                     
-                    <div class="modal-footer">
-                    <Link to='/menu'>
-                      <button
-                        type="button"
-                        class="btn btn-light"
-                        data-bs-dismiss="modal"
-                       
-                      >
-                        De acuerdo
-                      </button>
-                      </Link>
-                    </div>
+          
                   </div>
                 </div>
               </div>
